18ea078246cdbab185158ef711d8b76c58d8b217
Dimas Abreu Archanjo Dutra <dimas.dutra@mail.wvu.edu>
2025-08-05 12:04:35 -0400

    Finished for loops.

--- code/ForExample.ino
+++ code/ForExample.ino
@@ -1,10 +1,8 @@
 void setup() {
   Serial.begin(115200); // Initialize the serial port
 
-  for (int i=1; i<=10; i++) {
+  for (int i=1; i<=10; i++)
     Serial.println(i);
-    i++;
-  }
 }
 
 void loop() {
